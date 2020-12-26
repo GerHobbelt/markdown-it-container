fbacc5d4d02734f87fe480e346d9defb0d273e95
Ger Hobbelt <ger@hobbelt.com>
2020-12-26 16:23:08 +0100

    bumped build revision

--- dist/markdownItContainer.modern.js
+++ dist/markdownItContainer.modern.js
@@ -1,4 +1,4 @@
-/*! markdown-it-container 3.0.0-6 https://github.com//GerHobbelt/markdown-it-container @license MIT */
+/*! markdown-it-container 3.0.0-7 https://github.com//GerHobbelt/markdown-it-container @license MIT */
 
 // Process block-level custom containers
 //
